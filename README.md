971ab5a3d947390d768856c6b658605600322619
AinTEAsports <94745438+AinTEAsports@users.noreply.github.com>
2021-11-20 19:47:52 +0100

    Added main.py file and modified README file

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-Hello.
-
-This is my first "usefull" project so please be indulgent.
-
-This project is a password manager, but above all a container I tried to secure.
-
-Hope it's not too bad and enjoy.
+# ENGLISH :
+#
+# Umbrella password container, made by AinTea#0519, if you don't credit me or if you share this code without this
+# credit then you're evil and be evil is not cool
+#
+# This code principally create a file with your hashed password (to verify if you entered the good password) 
+# and it creates a container crypted (with XOR crypt algorythm) with your password stored as JSON data
+#
+# It's ot 100% secured you must know it I'm a beginner...
+# If you want to secure it you can change the hash method or just have a more secured password because you "easily" can crack
+# your password by brute force or dictionnary attack you can try ON YOUR CONTAINER btw because on others it's purely illegal
+# If your passwords has been hacked I'm not responsible I warned you
+#
+# ##################### ##################### ############# ################ ############################## #################
+#
+# FRANCAIS :
+#
+# Conteneur de mot de passe a peu près sécurisé, fait par AinTea#0519, si vous ne me creditez pas ou que vous partagez ce
+# code sans ce crédit bah vous serez méchant et c'est pas gentil d'etre mechant ("Par le pouvoir de la conjonctivite"
+# pour les connaisseurs)
+#
+# En gros ce code va créer un fichier mot de passe hashé pour verifier si vous avez entré le bon mot de passe
+# et ca créé un fichier conteneur qui va stocker vos mots de passe sous format JSON mais crypté par un chiffrage XOR
+#
+# C'est clairement pas sécurisé a 100% hein j'suis un débutant là dedans donc je préfère prévenir
+# Si vous voulez plus le sécuriser vous pouvez déjà renforcer la méthode de hashage en rajoutant des parties de hash par
+# ci par là et/ou avoir un mot de passe plus fort parce que pour le cracker il "suffit" d'utiliser une methode de force brute
+# vous pouvez essayer SUR LE VOTRE évidemment sinon c'est juste illegal
+# Si vos mots de passe sont hackés je ne suis en aucun responsable je vous ai prévenu au dessus
--- main.py
+++ main.py
@@ -9,22 +9,25 @@
 # It's ot 100% secured you must know it I'm a beginner...
 # If you want to secure it you can change the hash method or just have a more secured password because you "easily" can crack
 # your password by brute force or dictionnary attack you can try ON YOUR CONTAINER btw because on others it's purely illegal
+# If your passwords has been hacked I'm not responsible I warned you
 #
-# ##################### ##################### ############# ################
+# ##################### ##################### ############# ################ ############################## #################
 #
 # FRANCAIS :
 #
-# Conteneur de mot de passe a peu pres sécurisé, fait par AinTea#0519, si vous me creditez pas ou que vous partagez ce
-# code sans me crediter bah vous serez mechant et c'est pas gentil d'etre mechant (JEEJ pour les connaisseurs)
+# Conteneur de mot de passe a peu près sécurisé, fait par AinTea#0519, si vous ne me creditez pas ou que vous partagez ce
+# code sans ce crédit bah vous serez méchant et c'est pas gentil d'etre mechant ("Par le pouvoir de la conjonctivite"
+# pour les connaisseurs)
 #
-# En gros ce code va creer un fichier mot de passe hashé pour verifier si vous avez entré le bon mot de passe
+# En gros ce code va créer un fichier mot de passe hashé pour verifier si vous avez entré le bon mot de passe
 # et ca créé un fichier conteneur qui va stocker vos mots de passe sous format JSON mais crypté par un chiffrage XOR
 #
-# C'est clairement pas sécurisé a 100% hein j'suis un débutant la dedans donc je prefere prevenir
-# Si vous voulez plus le sécuriser vous pouvez deja renforcer la methode de hashage en rajoutant des parties de hash par
-# ci par la ou/et avoir un mot de passe plus fort parce que pour le cracker il "suffit" d'utiliser une methode de force brute
-# vous pouvez essayer SUR LE VOTRE evidemment sinon c'est juste illegal
-#
+# C'est clairement pas sécurisé a 100% hein j'suis un débutant là dedans donc je préfère prévenir
+# Si vous voulez plus le sécuriser vous pouvez déjà renforcer la méthode de hashage en rajoutant des parties de hash par
+# ci par là et/ou avoir un mot de passe plus fort parce que pour le cracker il "suffit" d'utiliser une methode de force brute
+# vous pouvez essayer SUR LE VOTRE évidemment sinon c'est juste illegal
+# Si vos mots de passe sont hackés je ne suis en aucun responsable je vous ai prévenu au dessus
+
 
 
 import os
