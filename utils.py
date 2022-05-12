c40bce105fc56da7bd0b8773674ca7dc823af6a6
AinTEAsports <benkirat.khalil@gmail.com>
2022-05-12 12:08:09 +0200

    utils.py : added colorString class for ascii art and added some methods

--- utils.py
+++ utils.py
@@ -1,5 +1,10 @@
 import os
+import json
 import hashlib
+
+import colorama
+import termcolor
+
 
 
 class Container:
@@ -120,7 +125,91 @@ class Container:
             return fileExists
 
 
+    def setPassword(self, password : str) -> None :
+        self.__password = password
 
-if __name__ == "__main__":
-    print("Chuck Norris has no dad, we do not f*ck Chuck Norris mother")
+
+    def isReadable(self) -> bool :
+        try:
+            with open(self.__filename, 'r') as f:
+                json.load(f)
+                
+                readable = True
+        except json.decoder.JSONDecodeError:
+            readable = False
+        except UnicodeDecodeError:
+            readable = False
+        finally:
+            return readable
+
+
+    def updateDict(self, newDict : dict) -> None :
+        if not self.isReadable():
+            raise OSError("I can't read the file, you mabe forgot to decrypt it first")
+
+
+    def createContainer(self) -> None :
+        if self.exists():
+            raise FileExistsError(f"container '{self.__filename}' already exists")
+        
+        with open(self.__filename, 'w') as f:
+            f.write("{}")
+
+
+
+class ColoredString:
+
+    def __init__(self, string: str = "") -> None :
+        self.string = string
+
+        self.__colors = {
+            "red" : colorama.Fore.RED,
+            "lightred" : colorama.Fore.LIGHTRED_EX,
+
+            "green" : colorama.Fore.GREEN,
+            "lightgreen" : colorama.Fore.LIGHTGREEN_EX,
+
+            "blue" : colorama.Fore.BLUE,
+            "lightblue" : colorama.Fore.LIGHTBLUE_EX,
+
+            "white" : colorama.Fore.WHITE,
+            "lightwhite" : colorama.Fore.LIGHTWHITE_EX,
+
+            "black" : colorama.Fore.BLACK,
+            "lightblack" : colorama.Fore.LIGHTBLACK_EX,
+
+            "cyan" : colorama.Fore.CYAN,
+            "lightcyan" : colorama.Fore.LIGHTCYAN_EX,
+
+            "yellow" : colorama.Fore.YELLOW,
+            "lightyellow" : colorama.Fore.LIGHTYELLOW_EX,
+
+            "magenta" : colorama.Fore.MAGENTA,
+            "lightmagenta" : colorama.Fore.LIGHTMAGENTA_EX,
+        }
+
+
+    def color(self, color : str, fromwhere : int = 0, toWhere : int = -1, changeDefaultString : bool = False) -> str :
+        if not self.colorExists(color):
+            raise ValueError(f"Color '{color}' does not exists, to check if a color exits, you can check the method 'colorExits'")
+
+        coloredstr = f"{self.__colors[color]}{self.string}{colorama.Fore.RESET}"
+
+        if changeDefaultString:
+            self.string = coloredstr
+
+        return coloredstr
+
+
+    def colorExists(self, color: str) -> bool :
+        return color in self.colorList()
+
+
+    def colorList(self) -> list[str] :
+        return [color for color in self.__colors.keys()]
+
+
+    def show(self) -> None :
+        print(self.string)
+
 
