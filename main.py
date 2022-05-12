493069590dd37aebebc1fdb5f05304e62e176a4c
AinTEAsports <benkirat.khalil@gmail.com>
2022-05-12 12:22:07 +0200

    main.py : Added switch case, ascii art and a while loop,next step is do what user wants

--- main.py
+++ main.py
@@ -1,13 +1,70 @@
 import os
 import sys
+import time
 
 import termcolor
 
-from containerClass import Container
+from utils import Container, ColoredString
+
+
+
+asciiText = """
+██╗   ██╗███╗   ███╗██████╗ ██████╗ ███████╗██╗     ██╗      █████╗ 
+██║   ██║████╗ ████║██╔══██╗██╔══██╗██╔════╝██║     ██║     ██╔══██╗
+██║   ██║██╔████╔██║██████╔╝██████╔╝█████╗  ██║     ██║     ███████║
+██║   ██║██║╚██╔╝██║██╔══██╗██╔══██╗██╔══╝  ██║     ██║     ██╔══██║
+╚██████╔╝██║ ╚═╝ ██║██████╔╝██║  ██║███████╗███████╗███████╗██║  ██║
+ ╚═════╝ ╚═╝     ╚═╝╚═════╝ ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝
+""".center(20)
+
+asciiText = ColoredString(asciiText)
+asciiText.color('lightgreen', changeDefaultString=True)
+
+asciiText.show()
 
 
 container = Container('container.crypt')
 
+if not container.exists():
+    print("[+] The container do not exists, creating it...\n")
+    container.createContainer()
 
+
+OPTIONS = """
+0- Quit
+1- Encrypt/Decrypt container
+2- Add passwords
+3- Change passwords
+4- Delete passwords
+5- Show all passwords
+6- Delete all passwords
+"""
+
+
+while True:
+    print(OPTIONS)
+    userChoice = input("Enter your choice : ")
+
+
+    match userChoice:
+        case '0':
+            # verifier que le contaihner a bien ete encrypt sinon proposer a l'utlisateur de l'encrypt
+            print("\n[+] Cleaning a little bit...\n")
+            time.sleep(1.5)
+            sys.exit()
+        case '1':
+            pass
+        case '2':
+            pass
+        case '3':
+            pass
+        case '4':
+            pass
+        case '5':
+            pass
+        case '6':
+            container.reinitialize(forceReinitalize=True)
+        case _:
+            print("\n---| INVALID CHOICE |---")
 
 
